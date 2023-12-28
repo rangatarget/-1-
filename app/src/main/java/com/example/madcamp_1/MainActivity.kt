e5cac53c6d3639252aa041eda4972047064346d6
es10130813 <110696075+es10130813@users.noreply.github.com>
2023-12-28 16:44:25 +0900

    탭레이아웃 및 프래그먼트 생성

--- app/src/main/java/com/example/madcamp_1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_1/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.madcamp_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.madcamp_1.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    val firstFragment = FirstFragment()
+    val secondFragment = SecondFragment()
+    val thirdFragment = ThirdFragment()
+
+    val fragments = arrayListOf<Fragment>(firstFragment, secondFragment, thirdFragment)
+    private val tabTextList = listOf("TAB1","TAB2","TAB3")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainViewpager2.adapter = ViewPager2Adapter(this)
+
     }
+
+
+
 }
+
